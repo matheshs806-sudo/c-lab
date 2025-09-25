04f3485b5a300ccf91b0470ad127edde05928acc
matheshs806-sudo <matheshs806@gmail.com>
2025-09-25 00:35:40 -0700

    Update eqnroot.cpp

--- eqnroot.cpp
+++ eqnroot.cpp
@@ -3,18 +3,17 @@
 int main() {
     // Equation: x^2 - 5x + 6 = 0
     int x;
-    int found = 0;   // use int instead of bool in C
+    int found = 0;   
 
     printf("Finding at least one root of the equation x^2 - 5x + 6 = 0\n");
 
-    // Try values of x from -100 to 100
     for (x = -100; x <= 100; x++) {
         int result = x * x - 5 * x + 6;
 
         if (result == 0) {
             printf("One root found: x = %d\n", x);
             found = 1;
-            break;  // stop after finding the first root
+            break;  
         }
     }
 
@@ -24,3 +23,4 @@ int main() {
 
     return 0;
 }
+
